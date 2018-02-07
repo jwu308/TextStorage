2008640d058ef5c48ed93215315443d80526c331
Kwanghee Kim <kkim682@gatech.edu>
2018-02-07 12:12:25 -0500

    re-upload

--- TextStorage/ViewController.swift
+++ TextStorage/ViewController.swift
@@ -8,15 +8,48 @@
 
 import UIKit
 
-// import for 
+// import for firebase google login
 import Firebase
+import FirebaseDatabase
 import GoogleSignIn
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GIDSignInUIDelegate {
+    
+    let textRef = Database.database().reference().child("temp")
+    
+    @IBAction func signin(_ sender: Any) {
+        GIDSignIn.sharedInstance().signIn()
+    }
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBAction func register(_ sender: Any) {
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (user, error) in
+            
+            if(error != nil){return}
+            
+            let alert = UIAlertController(title: "alert", message: "success to sign in", preferredStyle: UIAlertControllerStyle.alert)
+            
+            alert.addAction(UIAlertAction(title: "ok", style: UIAlertActionStyle.default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var display: UILabel!
+    @IBAction func uploadButton(_ sender: Any) {
+        textRef.setValue(inputField.text!)
+        textRef.observe(.value) {(snap: DataSnapshot) in
+            self.display.text = snap.value as? String
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        GIDSignIn.sharedInstance().uiDelegate = self
     }
 
     override func didReceiveMemoryWarning() {
